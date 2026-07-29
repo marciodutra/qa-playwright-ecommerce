712a0f1a228635b31df77afbb64a6d6649cc8300
Marcio Dutra <professormarciodutra@gmail.com>
2026-07-29 18:48:20 -0300

    fix environment variables fallback

--- src/helpers/environment.ts
+++ src/helpers/environment.ts
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const environment = {
-  baseUrl: process.env.BASE_URL ?? '',
-  apiUrl: process.env.API_URL ?? '',
+  baseUrl:
+    process.env.BASE_URL || 'https://www.demoblaze.com',
+
+  apiUrl:
+    process.env.API_URL || 'https://dummyjson.com',
 };
